36792168f28b0fc45b85d1245dbd0f3b8431ac7a
nikitinilya <Nikitin_ilya@outlook.com>
2018-06-16 14:31:08 +0500

    Update DeleteEventProcessor.php
    add delete event processor impl

--- daemon/src/EventProcessors/DeleteEventProcessor.php
+++ daemon/src/EventProcessors/DeleteEventProcessor.php
@@ -6,6 +6,28 @@ class DeleteEventProcessor extends BaseEventProcessor
 {
     public function process(): void
     {
-        // TODO: Implement process() method.
+        if (!$this->eventInfo['changedRows']
+            || empty($this->eventInfo['values'])
+            || empty($this->eventInfo['tableMap'])
+            || !\count($this->eventInfo['values'])
+        ) {
+            return;
+        }
+
+        /** @var TableMap $tableMap */
+        $tableMap = $this->eventInfo['tableMap'];
+        /** @var InvalidateRule[] $rules */
+        $rules = $this->getRulesFromDependencies($tableMap);
+
+        foreach ($rules as $rule) {
+            $warmer = $this->getWarmerBy($rule->getHashName());
+
+            if ($rule->isSingleKey() || $rule->hasPrimaryKey()) {
+                $pk = $rule->getPrimaryKey();
+                $id = $this->eventInfo['values'][0][$pk];
+                error_log("delete with " . $pk . " = " . $id);
+                $warmer->delete($warmer->getKey([$pk => $id]));
+            }
+        }
     }
 }
